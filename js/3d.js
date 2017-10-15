bcdbff2070a07cd9bf1ccd77717b0e6728bc82dc
lucasgruwez <gruwez.lucas@gmail.com>
2017-10-15 14:46:08 +1100

    Fixed JS indents

--- js/3d.js
+++ js/3d.js
@@ -17,20 +17,20 @@ var scene = new THREE.Scene();
 // Define some functions
 
 function loadTextures() {
-    var loader = new THREE.TextureLoader()
-    
-    moonColor   = loader.load('/moon/img/moon-color-2048.jpg')
-    moonNormal  = loader.load('/moon/img/moon-normal-2048.jpg')
-    moonBlurMap = loader.load('/moon/img/moon-blur.png')
+  var loader = new THREE.TextureLoader()
+
+  moonColor   = loader.load('/moon/img/moon-color-2048.jpg')
+  moonNormal  = loader.load('/moon/img/moon-normal-2048.jpg')
+  moonBlurMap = loader.load('/moon/img/moon-blur.png')
 }
 
 loadTextures()
 var moonG = new THREE.IcosahedronGeometry(100, 4);
 var moonM = new THREE.MeshStandardMaterial({
-    map: moonColor,
-    normalMap: moonNormal,
-    normalScale: new THREE.Vector3( 0.08, 0.08 ),
-    roughness: 1
+  map: moonColor,
+  normalMap: moonNormal,
+  normalScale: new THREE.Vector3( 0.08, 0.08 ),
+  roughness: 1
 });
 var moon  = new THREE.Mesh(moonG, moonM);
 moon.rotation.z = Math.PI / 2
@@ -39,8 +39,8 @@ scene.add(moon);
 
 var moonBlurG = new THREE.PlaneGeometry(200, 80);
 var moonBlurM = new THREE.MeshBasicMaterial({
-    map: moonBlurMap,
-    transparent: true
+  map: moonBlurMap,
+  transparent: true
 })
 var moonBlur = new THREE.Mesh(moonBlurG, moonBlurM);
 moonBlur.position.set(0, -10, -95)
@@ -51,16 +51,16 @@ light.position.set(0, 1000, -1400)
 scene.add(light)
 
 function resize() {
-    var w = window.innerWidth;
-    var h = 600;
-    cnvs.style.width = w + 'px';
-    cnvs.style.height = h + 'px';
-  
-    // Update camera and renderer
-  
-    renderer.setSize(w, h);
-    camera.aspect = w / h;
-    camera.updateProjectionMatrix();
+  var w = window.innerWidth;
+  var h = 600;
+  cnvs.style.width = w + 'px';
+  cnvs.style.height = h + 'px';
+
+  // Update camera and renderer
+
+  renderer.setSize(w, h);
+  camera.aspect = w / h;
+  camera.updateProjectionMatrix();
 }
 
 window.addEventListener('resize', resize)
